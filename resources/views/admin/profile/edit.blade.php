e19c8801d136a0d2ceb3508b1252fb44a41cb61a
SATO <gogo4641kinucivic@gmail.com>
2019-11-20 09:05:50 +0000

    first commit

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Information</title>
+        <title>Profile</title>
     </head>
     <body>
-        <h1>Information2作成画面</h1>
+        <h1>Profile編集</h1>
     </body>
 </html>
